5c4798945038c191043a2e12058700d5d55bdcdf
Saket kumar <kumarsaket601@gmail.com>
2024-03-07 21:44:45 +0530

    add context

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { CryptoProvider } from "./context/CryptoContext.jsx";
+import { TrendingProvider } from "./context/TrendingContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-    
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <TrendingProvider>
+      <CryptoProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CryptoProvider>
+    </TrendingProvider>
+  </React.StrictMode>
+);
