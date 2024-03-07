9f145c06ef2e9649702c1f06696e820a97ec9fe6
Saket kumar <kumarsaket601@gmail.com>
2024-03-07 21:34:41 +0530

    add component

---
+++ src/component/Tokonomics.jsx
@@ -0,0 +1,58 @@
+const Tokonomics = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold text-left ">Tokenomics</h2>
+      <h3 className="font-semibold text-xl py-4">Initial Distribution</h3>
+      <div className="flex items-center gap-4">
+        <svg
+          width="179"
+          height="180"
+          viewBox="0 0 179 180"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle
+            cx="89.5"
+            cy="89.7969"
+            r="74.5"
+            stroke="#0082FF"
+            strokeWidth="30"
+          />
+          <mask id="path-2-inside-1_1_922" fill="white">
+            <path d="M89.5 0.296875C74.3468 0.296875 59.4417 4.14426 46.1817 11.4784C32.9217 18.8126 21.7409 29.3935 13.6873 42.2293C5.63363 55.065 0.970851 69.7355 0.135952 84.8657C-0.698948 99.9958 2.32137 115.09 8.91382 128.734L89.5 89.7969V0.296875Z" />
+          </mask>
+          <path
+            d="M89.5 0.296875C74.3468 0.296875 59.4417 4.14426 46.1817 11.4784C32.9217 18.8126 21.7409 29.3935 13.6873 42.2293C5.63363 55.065 0.970851 69.7355 0.135952 84.8657C-0.698948 99.9958 2.32137 115.09 8.91382 128.734L89.5 89.7969V0.296875Z"
+            stroke="#FAA002"
+            strokeWidth="60"
+            mask="url(#path-2-inside-1_1_922)"
+          />
+          <circle cx="89.5" cy="89.5" r="59.5" fill="white" />
+          <circle cx="89.5" cy="89.5" r="59.5" stroke="#0082FF" />
+        </svg>
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-2 items-center">
+            <div className="w-4 h-4 rounded-full bg-blue-500"></div>
+            <span>Crowdsale investors: 80%</span>
+          </div>
+          <div className="flex gap-2 items-center">
+            <div className="w-4 h-4 rounded-full bg-yellow-400"></div>
+            <span>Foundation: 20%</span>
+          </div>
+        </div>
+      </div>
+      <p className="py-4">
+        Lorem ipsum dolor sit amet consectetur. Cras aliquet tristique ornare
+        vestibulum nunc dignissim vel consequat. Leo etiam nascetur bibendum
+        amet enim sit eget leo amet. At metus orci augue fusce eleifend lectus
+        eu fusce adipiscing. Volutpat ultrices nibh sodales massa habitasse urna
+        felis augue. Gravida aliquam fermentum augue eu. Imperdiet bibendum amet
+        aliquam donec. Eget justo dui metus odio rutrum. Vel ipsum eget in at
+        curabitur sem posuere facilisis vitae. Sed lorem sit mauris id eget arcu
+        ut. Vulputate ipsum aliquet odio nisi eu ac risus.
+      </p>
+    </div>
+  );
+};
+
+export default Tokonomics;
